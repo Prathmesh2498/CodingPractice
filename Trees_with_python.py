f26ee561d0160ca4b5cd9989c8645a2f1b486654
Prathmesh2498 <prathu10@gmail.com>
2020-06-16 10:48:48 +0530

    Added Level Order Search to trees code

--- Trees_with_python.py
+++ Trees_with_python.py
@@ -40,6 +40,31 @@ class BinaryTree:
          BinaryTree.postorderTraversal(head.left)
          BinaryTree.postorderTraversal(head.right)
          print(head.data)
+     
+    @staticmethod
+    def printLevelOrder(root): 
+        # Base Case 
+        if root is None: 
+            return
+          
+        # Create an empty queue for level order traversal 
+        queue = [] 
+      
+        # Enqueue Root and initialize height 
+        queue.append(root) 
+      
+        while(len(queue) > 0): 
+            # Print front of queue and remove it from queue 
+            print (queue[0].data) 
+            node = queue.pop(0) 
+      
+            #Enqueue left child 
+            if node.left is not None: 
+                queue.append(node.left) 
+      
+            # Enqueue right child 
+            if node.right is not None: 
+                queue.append(node.right) 
             
     def addNode(self,head,data):
         #For the first Node
